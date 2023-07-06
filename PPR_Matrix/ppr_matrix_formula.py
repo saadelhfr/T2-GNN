54fa3a22cc8155644cc8a1dc118eb4d9a1576105
saad el hafre <saad.el-hafre@polytechnique.edu>
2023-07-06 16:26:37 +0200

    4th commit

---
+++ PPR_Matrix/ppr_matrix_formula.py
@@ -0,0 +1,22 @@
+import torch 
+
+
+def calculate_ppr_matrix(A,  device,  alpha=0.15, tol=1e-6):
+    
+    alpha = torch.tensor(alpha, dtype=torch.float, device=device)
+    I = torch.eye(A.size(0), device=device)
+    
+    # Create a normalized adjacency matrix
+    deg_inv_sqrt = torch.rsqrt(A.sum(dim=1))
+    A_hat = A * deg_inv_sqrt.unsqueeze(1) * deg_inv_sqrt.unsqueeze(0)
+    A_hat = A_hat + I  # add self-loops
+
+    # Initial guess for P
+    P = torch.full(A_hat.shape, 1/A.size(0), device=device)
+    diff = float('inf')
+    while diff > tol:
+        new_P = (1 - alpha) * torch.mm(A_hat, P) + alpha * I
+        diff = torch.abs(P - new_P).sum()
+        P = new_P
+
+    return P
