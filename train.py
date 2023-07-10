5ade8dadd8c36042bf6c261ba66790a66d20a4b9
saad el hafre <saad.el-hafre@polytechnique.edu>
2023-07-10 12:17:01 +0200

    checkpoints

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from .model import Teacher_Features , Teacher_Edge , Student
 from sklearn.cluster import KMeans , SpectralClustering , AgglomerativeClustering , DBSCAN , OPTICS , Birch
 from .utils import students_t_kernel_euclidean , student_t_kernel_cosine , generate_targer_distribution
 from .PPR_Matrix.ppr import topk_ppr_matrix
+import os 
 
 class Train : 
     def __init__(self , epochs ,  device  , num_nodes, input_dim , output_dim , hidden2 , nbr_clusters , teta = 0.5 ,dropout_rate=0.2 , lr=0.001 , clustering_method='kmeans'):
@@ -36,7 +37,19 @@ class Train :
 
         self.clustering_method = clustering_method
 
+    """
+    Pretrain the feeature teacher 
+    """
+
     def pretrain_feat_teacher(self , feat_epochs , data ) :
+        loadcheck = input("Do you want to load a checkpoint ? (y/n) : ")
+        if loadcheck == 'y' :
+            self.load_checkpoint(model='feat_teacher')
+            print("Feature teacher checkpoint loaded")
+        else :
+            print("Training from scratch")
+
+
         adj = data.adj
         X = data.x
         self.feature_teacher.train()
@@ -65,8 +78,21 @@ class Train :
             loss.backward()
             self.feat_teach_optimizer.step()
             print("Epoch : {} , Loss : {}".format(epoch , loss.item()))
+            if epoch % 10 == 0 :
+                self.save_checkpoint(epoch , model='feat_teacher')
+                print("Feature teacher checkpoint saved at epoch {}".format(epoch))
+
+    """
+    pretrain the edge teacher 
+    """
 
     def pretrain_edge_teacher(self , edge_epochs , data) :
+        loadcheck = input("Do you want to load a checkpoint ? (y/n) : ")
+        if loadcheck == 'y' :
+            self.load_checkpoint(model='feat_teacher')
+            print("Feature teacher checkpoint loaded")
+        else :
+            print("Training from scratch")
         self.edge_teacher.train()
         X = data.x
         adj_aug = data.adj_aug
@@ -97,8 +123,22 @@ class Train :
             loss.backward()
             self.edge_teach_optimizer.step()
             print("Epoch : {} , Loss : {}".format(epoch , loss.item()))
+            if epoch % 10 == 0 :
+                self.save_checkpoint(epoch , model='edge_teacher')
+                print("Edge teacher checkpoint saved at epoch {}".format(epoch))
+
+
+    """
+    given a feature teacher and an edge/structure teacher train the student
+    """
 
     def train_student(self , student_epochs , data):
+        loadcheck = input("Do you want to load a checkpoint ? (y/n) : ")
+        if loadcheck == 'y' :
+            self.load_checkpoint(model='feat_teacher')
+            print("Feature teacher checkpoint loaded")
+        else :
+            print("Training from scratch")
         X = data.x
         adj = data.adj
         adj_aug = data.adj_aug
@@ -132,13 +172,44 @@ class Train :
             loss.backward()
             self.student_optimizer.step()
             print("Epoch : {} , Loss : {}".format(epoch , loss.item()))
+            if epoch % 10 == 0 :
+                self.save_checkpoint(epoch , model='student')
+                print("Student checkpoint saved at epoch {}".format(epoch))
 
 
 
+    """
+    Save checkpoint
+    """
 
+    def save_checkpoint(self , epoch :int , path = "/Data/T2_GNN/save_checkpoint/" , model = 'student' ):
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-
-
-
+        if model == 'student':
+            torch.save(self.student.state_dict() , path+'_student.pth')
+        elif model == 'feat_teacher':
+            torch.save(self.feature_teacher.state_dict() , path+'_feat_teacher.pth')
+        elif model == 'edge_teacher':
+            torch.save(self.edge_teacher.state_dict() , path+'_edge_teacher.pth')
+        else : 
+            raise ValueError("Model must be student or feat_teacher or edge_teacher")
         
+    """
+    Load checkpoint
+    """
+
+    def load_checkpoint(self , path = "/Data/T2_GNN/save_checkpoint/" , model = 'student'):
+        if not os.path.exists(path):
+            print("No checkpoint found")
+            print("Training from scratch")
+            return
         
+        if model == 'student':
+            self.student.load_state_dict(torch.load(path+'_student.pth'))
+        elif model == 'feat_teacher':
+            self.feature_teacher.load_state_dict(torch.load(path+'_feat_teacher.pth'))
+        elif model == 'edge_teacher':
+            self.edge_teacher.load_state_dict(torch.load(path+'_edge_teacher.pth'))
+        else : 
+            raise ValueError("Model must be student or feat_teacher or edge_teacher")
