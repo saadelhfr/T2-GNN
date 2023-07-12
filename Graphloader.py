82dc49ab396a2c9a18bef40ce46ead5aa27970d4
saad el hafre <saad.el-hafre@polytechnique.edu>
2023-07-12 12:11:00 +0200

    graphloader update

--- Graphloader.py
+++ Graphloader.py
@@ -1,4 +1,7 @@
 import torch 
+from torch_geometric.utils import from_networkx
+from torch_geometric.data import Data ,  DataLoader 
+import networkx as nx
 
 
 """
@@ -8,6 +11,34 @@ the goal is to be able to calculate the adjancency matrix and the PPR matrix wit
 """
 
 
-class GraphLoader():
-    # This class is used to load the graph from the pickle file and divide it into batches based on the strongly connected  componenets 
-    pass
+class GraphLoader(DataLoader):
+    def __init__(self , load_path , save_path ,sampling_strategy, device ,directed :bool = False) :
+        self.load_path = load_path
+        self.save_path = save_path
+        self.sampling_strategy = sampling_strategy
+        self.directed = directed
+        self.device = device
+        self.list_subgraphs_path = []
+        
+
+    def load_nx_graph(self): 
+        self.graph=nx.read_gpickle(self.load_path)
+        self.nbr_nodes = self.graph.number_of_nodes()
+
+    def save_torch_geo_subgraph(self , subgraph , path):
+        torch.save(subgraph , path)
+        self.list_subgraphs_path.append(path)
+
+
+    def sample_subgraphs(self):
+
+        if self.sampling_strategy == "connected_components" : 
+            assert self.directed == False , "Connected components sampling strategy is only available for undirected graphs"
+            self.subgraphs = list(nx.connected_components(self.graph))
+            self.nbr_nodes_subgraphs = [len(subgraph) for subgraph in self.subgraphs]
+            self.nbr_subgraphs = len(self.subgraphs)
+
+        else : 
+            raise NotImplementedError("Sampling strategy not implemented")
+
+
